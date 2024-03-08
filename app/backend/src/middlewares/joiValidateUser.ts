1750bedd865f0a196590609fb84c0e3d5b66b90d
erica <soareserika3724@gmail.com>
2024-03-08 16:25:34 -0300

    fluxo 2

--- app/backend/src/middlewares/joiValidateUser.ts
+++ app/backend/src/middlewares/joiValidateUser.ts
@@ -4,7 +4,7 @@ import Joi = require('joi');
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
 
 const validate = (req : Request, res: Response, next: NextFunction) => {
