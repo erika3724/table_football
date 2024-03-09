5868b08bf3010e15501ce60ac4d5236016fc2e26
Erika Ribeiro <122754392+erika3724@users.noreply.github.com>
2024-03-09 19:46:52 -0300

    Revert "fluxo 2"
    This reverts commit 5b17d80f0547c7443f6833bc218ad4e6c915f0fe.

--- app/backend/src/controllers/loginControler.ts
+++ app/backend/src/controllers/loginControler.ts
@@ -5,9 +5,12 @@ import login from '../service/login.service';
 class loginController {
   static async getoLogin(req: Request, res: Response) {
     try {
-      const { email } = req.body;
-      const response = await login.getLogin(email);
-      const code = jwt(response as string);
+      const { email, password } = req.body;
+      const response = await login.getLogin(email, password);
+      if (response === 'error invalid') {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+      const code = jwt(response);
       res.status(200).json({ token: code });
     } catch (error) {
       res.status(500).json({ error: 'Internal Server Error' });
