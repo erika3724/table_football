fde255a84564f1e3af8b656532ce6f87415d77ff
Erika Ribeiro <122754392+erika3724@users.noreply.github.com>
2024-03-09 19:53:53 -0300

    tentativa

--- app/backend/src/middlewares/joiValidateUser.ts
+++ app/backend/src/middlewares/joiValidateUser.ts
@@ -5,15 +5,24 @@ import user from '../database/models/user.model';
 
 const loginSchema = Joi.object({
   email: Joi.string().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().required(),
 });
 
 const validate = async (req : Request, res: Response, next: NextFunction) => {
   const { error } = loginSchema.validate(req.body);
   const emailRegex = /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/;
   const a = emailRegex.test(req.body.email);
-  if (error || a === false) {
-    return res.status(401).json({ message: 'All fields must be filled' });
+  if (error) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  const b = await user.findOne({ where: { email: req.body.email } });
+
+  if (b === null || a === false || req.body.password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+  const m = await bcrypt.compare(req.body.password, b.dataValues.password);
+  if (m === false) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
 };
--- app/backend/src/middlewares/validateToken.ts
+++ app/backend/src/middlewares/validateToken.ts
@@ -1,18 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
+import UserModel from '../database/models/user.model';
 
+const e = 'Token must be a valid token';
 const validateToken = async (req: Request, res: Response, next: NextFunction) => {
-  const secretKey = process.env.SECRET_KEY || 'jwt_secret';
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(401).json({ message: 'Token not found' });
+  try {
+    const secretKey = process.env.SECRET_KEY || 'jwt_secret';
+
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+    const decodedToken = jwt.verify(req.headers.authorization.split(' ')[1], secretKey) as string;
+    if (!decodedToken) {
+      return res.status(401).json({ message: e });
+    }
+    const response = await UserModel.findByPk(decodedToken);
+    if (!response) {
+      return res.status(401).json({ message: e });
+    }
+    req.body.role = response.role;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: e });
   }
-  const decodedToken = jwt.verify(token, secretKey) as JwtPayload;
-  const response = await UserModel.findByPk(decodedToken.data.id);
-  if (!response) {
-    return res.status(401).json({ message: 'Token must be a valid token' });
-  }
-  next();
 };
 
 export default validateToken;
