06ab92231aff21cb818e62157b47735fa6945f75
Erika Ribeiro <122754392+erika3724@users.noreply.github.com>
2024-03-09 20:24:53 -0300

    a

--- app/backend/src/router/teams.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-import teamController from '../controllers/teamController';
-
-const router = Router();
-
-router.get('/', (req: Request, res:Response) => teamController.getAllTeams(req, res));
-router.get('/:id', (req: Request, res:Response) => teamController.getOneTeams(req, res));
-
-export default router;
